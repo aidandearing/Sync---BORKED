d01b8ad080c05b24e3b0ad84c343093e48aaade8
aidandearing <aidan.dearing@live.com>
2017-02-27 16:57:33 -0500

    Contra
    Contra has an animation controller with a splash state, a synchronism
    behaviour (currently only made for splash screen), and some basic UI.
    
    Contra is rigged, and the first animation has been made.

--- Sync/Sync/Assets/Scripts/Synchronisation/ContraSynchronism.cs
+++ Sync/Sync/Assets/Scripts/Synchronisation/ContraSynchronism.cs
@@ -10,27 +10,53 @@ public class ContraSynchronism : MonoBehaviour
     public Material contraMaterial;
     public GameObject discoBall;
     public Material discoBallMaterial;
+    public UnityStandardAssets.Utility.AutoMoveAndRotate autoMoveandRotate;
+    public Animator controller;
+    public UnityStandardAssets.ImageEffects.EdgeDetection edgeShader;
+
+    [Header("Sequencers")]
+    public SequencerGradient[] sequencers;
 
     [Header("Emission")]
-    public SequencerGradient emissionSequencer;
     public Gradient emissionGradient;
 
     [Header("Disco Ball Emission")]
-    public SequencerGradient discoBallEmissionSequencer;
     public Gradient discoBallEmissionGradient;
 
-    void Start()
-    {
+    [Header("Auto Move and Rotate")]
+    public Vector3 autoMoveandRotateStart;
+    public Vector3 autoMoveandRotateEnd;
 
-    }
+    [Header("Controller")]
+    public AnimationCurve controllerSpeed;
+
+    [Header("Edge Detection")]
+    public Gradient edgeColour;
+    public AnimationCurve edgeThreshold;
+    public AnimationCurve edgeDistance;
+    public AnimationCurve edgeOnly;
+    public AnimationCurve edgeDepth;
+    public AnimationCurve edgeNormal;
 
     void Update()
     {
-        if (!emissionSequencer.isInitialised)
-            emissionSequencer.Initialise();
+        foreach (SequencerGradient sequencer in sequencers)
+        {
+            if (!sequencer.isInitialised)
+                sequencer.Initialise();
+        }
 
-        contraMaterial.SetColor("_EmissionColor", emissionGradient.Evaluate(emissionSequencer.Evaluate()));
-        discoBallMaterial.SetColor("_EmissionColor", discoBallEmissionGradient.Evaluate(discoBallEmissionSequencer.Evaluate()));
+        contraMaterial.SetColor("_EmissionColor", emissionGradient.Evaluate(sequencers[0].Evaluate()));
+        discoBallMaterial.SetColor("_EmissionColor", discoBallEmissionGradient.Evaluate(sequencers[1].Evaluate()));
+        autoMoveandRotate.rotateDegreesPerSecond.value = Vector3.Lerp(autoMoveandRotateStart, autoMoveandRotateEnd, sequencers[2].Evaluate());
+        controller.SetFloat("speedMultiplier", controllerSpeed.Evaluate(sequencers[3].Evaluate()));
+
+        edgeShader.edgesOnly = edgeOnly.Evaluate(sequencers[4].Evaluate());
+        edgeShader.edgesOnlyBgColor = edgeColour.Evaluate(sequencers[4].Evaluate());
+        edgeShader.lumThreshold = edgeThreshold.Evaluate(sequencers[4].Evaluate());
+        edgeShader.sampleDist = edgeDistance.Evaluate(sequencers[4].Evaluate());
+        edgeShader.sensitivityDepth = edgeDepth.Evaluate(sequencers[4].Evaluate());
+        edgeShader.sensitivityNormals = edgeNormal.Evaluate(sequencers[4].Evaluate());
     }
 
     void FixedUpdate()
