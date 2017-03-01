1d1cd14be8395e8b8fd88fd71661fcdf0c32728f
aidandearing <aidan.dearing@live.com>
2017-03-01 01:57:49 -0500

    Nero
    Made much progress on Nero, then some set backs.
    I have re rigged Nero as per requested by the unity rig tool, with that
    done I now have to redo all the animations, which so far included the
    splash animation and the first idle animation, nothing substantial.

---
+++ Sync/Sync/Assets/Scripts/Characters/Nero/NeroGraffitiBehaviour.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class NeroGraffitiBehaviour : MonoBehaviour
+{
+    [Header("References")]
+    [Tooltip("The Material this Graffiti object is going to clone on start, and modify based on Nero's distance from it.")]
+    public Material graffiti;
+    new public Renderer renderer;
+
+    [Header("Colour")]
+    public Gradient graffitiColour;
+
+    [Header("Cutout")]
+    public AnimationCurve graffitiCutout;
+
+    [Header("Emission")]
+    public Gradient graffitiEmission;
+
+    [Header("Distance")]
+    [Range(0.0f, 1.0f)]
+    public float distance;
+
+    private NeroGraffitiControl controller;
+
+    void Start()
+    {
+        renderer.material = Instantiate(graffiti);
+    }
+
+    void Update()
+    {
+        //if (controller == null)
+        //{
+        //    gameObject.SetActive(false);
+        //    return;
+        //}
+
+        distance = (controller != null) ? controller.Evaluate(gameObject.transform) : 0.0f;
+
+        renderer.material.SetColor("_Color", graffitiColour.Evaluate(distance));
+        renderer.material.SetFloat("_Cutoff", graffitiCutout.Evaluate(distance));
+        renderer.material.SetColor("_EmissionColor", graffitiEmission.Evaluate(distance));
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+
+        if (other.tag == StringLiterals.Tags.NeroGraffitiController)
+        {
+            NeroGraffitiControl nero = other.gameObject.GetComponentInChildren<NeroGraffitiControl>();
+            controller = nero;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == StringLiterals.Tags.NeroGraffitiController)
+        {
+            controller = null;
+        }
+    }
+}
---
+++ Sync/Sync/Assets/Scripts/Characters/Nero/NeroGraffitiControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class NeroGraffitiControl : MonoBehaviour
+{
+    [Header("References")]
+    public Transform location;
+    new public SphereCollider collider;
+
+    [Header("Distance")]
+    public float nearDistance = 5.0f;
+    public float farDistance = 25.0f;
+
+    void Start()
+    {
+        collider.radius = farDistance;
+    }
+
+    public float Evaluate(Transform other)
+    {
+        float distance = Mathf.Clamp((other.position - location.position).magnitude, nearDistance, farDistance) / (farDistance - nearDistance);
+        return Mathf.Lerp(0, 1, 1 - distance); 
+    }
+}
